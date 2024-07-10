f7ba57ed0cbc1f82bf8f666e8b40102c5c30cb2f
Tuan Duy <htr.tduy2909@outlook.com.vn>
2024-07-11 01:17:40 +0700

    Update database connection to use environment variables

--- connect.php
+++ connect.php
@@ -1,21 +1,12 @@
 <?php
-// Lấy các biến môi trường
-$servername = getenv('viaduct.proxy.rlwy.net');
-$username = getenv('root');
-$password = getenv('EMlgsirgeLBFYkJpAIbqzosXFnvCmHeG');
-$dbname = getenv('railway');
+$host = getenv('MYSQLHOST');
+$db = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
 
-// Kiểm tra xem các biến môi trường đã được thiết lập hay chưa
-if (!$servername || !$username || !$password || !$dbname) {
-    die('Một hoặc nhiều biến môi trường không được thiết lập.');
-}
+$conn = new mysqli($host, $user, $pass, $db);
 
-// Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Kiểm tra kết nối
 if ($conn->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Kết nối thành công";
 ?>
