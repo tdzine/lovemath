9e991007bfe0151c94208d84a050af72b390bbbd
Tuan Duy <htr.tduy2909@outlook.com.vn>
2024-07-11 01:39:20 +0700

    Update with database connection settings

--- connect.php
+++ connect.php
@@ -3,8 +3,9 @@ $host = getenv('MYSQLHOST');
 $db = getenv('MYSQLDATABASE');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
