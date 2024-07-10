cbf4e85d881ff84541e15015eaa6b90d3b1bd84d
Tuan Duy <htr.tduy2909@outlook.com.vn>
2024-07-11 01:56:31 +0700

    Update with database connection settings

--- connect.php
+++ connect.php
@@ -1,9 +1,14 @@
 <?php
-$host = getenv('MYSQLHOST');
-$db = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['MYSQLHOST'];
+$db = $_ENV['MYSQLDATABASE'];
+$user = $_ENV['MYSQLUSER'];
+$pass = $_ENV['MYSQLPASSWORD'];
+$port = $_ENV['MYSQLPORT'];
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
